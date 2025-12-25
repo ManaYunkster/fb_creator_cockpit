ac4c8bd99f50f1a877aca3aade3526a82a2b597a
eric <eric@elucidate.ventures>
2025-12-25 19:04:47 +0000

    fix for react vulnerability

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,8 +1,14 @@
-{pkgs}: {
-  channel = "stable-24.05";
-  packages = [ pkgs.nodejs_22 ];
+{ pkgs, ... }: {
+  channel = "stable-23.11";
+  
+  packages = [ 
+    pkgs.nodejs_20  # Use underscore and a version available
+  ];
+
   idx.extensions = [ "svelte.svelte-vscode" "vue.volar" ];
+
   idx.previews = {
+    enable = true; # Required to activate the preview panel
     previews = {
       web = {
         command = [ "npm" "run" "dev" "--" "--port" "$PORT" "--host" "0.0.0.0" ];
